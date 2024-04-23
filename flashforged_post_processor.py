352077dc9e2a8354cebbf1e4dbb83519b0442b25
lukekuhner <lukekuhner@gmail.com>
2024-04-22 21:03:38 -0500

    Update flashforged_post_processor.py

--- flashforged_post_processor.py
+++ flashforged_post_processor.py
@@ -5,10 +5,14 @@ try:
     env_slicer_pp_output_name = str(os.getenv('SLIC3R_PP_OUTPUT_NAME'))
     #for s in sys.argv:
     #    print(s)
-    print(env_slicer_pp_output_name)
+    
     with open(sys.argv[1], "r") as f:
         read_file_lines = f.readlines()
-
+    
+    output_slices = env_slicer_pp_output_name.split(".")
+    env_slicer_pp_output_name = output_slices[0].replace(" ", "") + "_FIXED" + "." + output_slices[1]
+    #print(env_slicer_pp_output_name)
+    #input()
     with open(env_slicer_pp_output_name, "w" , encoding='UTF-8') as f:
         first_G1_removed = False
         for line in read_file_lines:
